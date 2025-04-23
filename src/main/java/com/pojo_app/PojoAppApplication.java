8f9bb54b939c5d0bb3f29467775d29a4930d79ca
H@rish <harishkurani79@gmail.com>
2025-04-23 20:15:17 +0530

    pojo-app-v1

---
+++ src/main/java/com/pojo_app/PojoAppApplication.java
@@ -0,0 +1,13 @@
+package com.pojo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojoAppApplication.class, args);
+	}
+
+}
