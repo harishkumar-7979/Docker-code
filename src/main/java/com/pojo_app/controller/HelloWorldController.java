59640fd00aa73d4da7f0823b697cf519a4ada391
harishkumar-7979 <kumarharish483425@gmail.com>
2025-04-24 17:36:53 +0530

    Update HelloWorldController.java

--- src/main/java/com/pojo_app/controller/HelloWorldController.java
+++ src/main/java/com/pojo_app/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/message")
     public String getMessage(){
-        return "hello world";
+        return "Hii harish kurani whatsup brou";
     }
 }
